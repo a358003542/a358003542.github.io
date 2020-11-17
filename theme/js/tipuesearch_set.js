2f7e21ce1b6647964c1f6fe6442956d60ade639e
wanze <a358003542@gmail.com>
2020-11-17 19:41:04 +0800

    优化了搜索过程

--- theme/js/tipuesearch_set.js
+++ theme/js/tipuesearch_set.js
@@ -1,11 +1,13 @@
 
 /*
-Tipue Search 6.1
-Copyright (c) 2017 Tipue
+Tipue Search 6.0
+Copyright (c) 2015-2016 Tipue
 Tipue Search is released under the MIT License
 http://www.tipue.com/search
 */
 
+// Start timer for search execution time. The value will be used in tipuesearch.js .
+var startTime = new Date().getTime();
 
 /*
 Stop words
@@ -18,7 +20,7 @@ var tipuesearch_stop_words = ["a", "about", "above", "after", "again", "against"
 // Word replace
 
 var tipuesearch_replace = {'words': [
-     {'word': 'tip', 'replace_with': 'tipue'},
+     {'word': 'tipua', 'replace_with': 'tipue'},
      {'word': 'javscript', 'replace_with': 'javascript'},
      {'word': 'jqeury', 'replace_with': 'jquery'}
 ]};
@@ -27,28 +29,18 @@ var tipuesearch_replace = {'words': [
 // Weighting
 
 var tipuesearch_weight = {'weight': [
-     {'url': 'http://www.tipue.com', 'score': 20},
-     {'url': 'http://www.tipue.com/search', 'score': 30},
-     {'url': 'http://www.tipue.com/is', 'score': 10}
+     {'url': 'http://www.tipue.com', 'score': 200},
+     {'url': 'http://www.tipue.com/search', 'score': 100},
+     {'url': 'http://www.tipue.com/about', 'score': 100}
 ]};
 
 
-// Illogical stemming
+// Stemming
 
 var tipuesearch_stem = {'words': [
      {'word': 'e-mail', 'stem': 'email'},
      {'word': 'javascript', 'stem': 'jquery'},
      {'word': 'javascript', 'stem': 'js'}
-]};
-
-
-// Related searches
-
-var tipuesearch_related = {'searches': [
-     {'search': 'tipue', 'related': 'Tipue Search'},
-     {'search': 'tipue', 'before': 'Tipue Search', 'related': 'Getting Started'},
-     {'search': 'tipue', 'before': 'Tipue', 'related': 'jQuery'},
-     {'search': 'tipue', 'before': 'Tipue', 'related': 'Blog'}
 ]};
 
 
@@ -59,22 +51,16 @@ var tipuesearch_string_2 = 'Showing results for';
 var tipuesearch_string_3 = 'Search instead for';
 var tipuesearch_string_4 = '1 result';
 var tipuesearch_string_5 = 'results';
-var tipuesearch_string_6 = 'Back';
-var tipuesearch_string_7 = 'More';
-var tipuesearch_string_8 = 'Nothing found.';
-var tipuesearch_string_9 = 'Common words are largely ignored.';
+var tipuesearch_string_6 = 'Previous';
+var tipuesearch_string_7 = 'Next';
+var tipuesearch_string_8 = 'Nothing found';
+var tipuesearch_string_9 = 'Common words are largely ignored';
 var tipuesearch_string_10 = 'Search too short';
-var tipuesearch_string_11 = 'Should be one character or more.';
+var tipuesearch_string_11 = 'Should be one character or more';
 var tipuesearch_string_12 = 'Should be';
-var tipuesearch_string_13 = 'characters or more.';
-var tipuesearch_string_14 = 'seconds';
-var tipuesearch_string_15 = 'Searches related to';
-
-
-// Internals
-
-
-// Timer for showTime
-
-var startTimer = new Date().getTime();
-
+var tipuesearch_string_13 = 'characters or more';
+var tipuesearch_string_14 = '&lt;&lt;';
+var tipuesearch_string_15 = '&gt;&gt;';
+var tipuesearch_string_16 = 'page(s) of results.';
+var tipuesearch_string_17 = 'Score:';
+var tipuesearch_string_18 = 'Search took $1 seconds.';
