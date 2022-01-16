eafdb95a5383f3cd48af4557c1c5cbefd8a9f1d0
wanze <a358003542@outlook.com>
2022-01-17 01:37:37 +0800

    update

--- theme/js/article.js
+++ theme/js/article.js
@@ -1,48 +1,58 @@
-function adjust_markdown_css(){
-    $('table').addClass('table table-striped');
+function add_table_class() {
+    for (let e of document.querySelectorAll('table')) {
+        e.classList.add('table', 'table-striped')
+    }
 }
 
 
-function footnote_popover(){
-    $('.footnote-ref').each(function(){
-        let footnote_index = $(this).text();
+function add_footnote_support() {
+    for (let e of document.querySelectorAll('.footnote-ref')) {
+        
+        let footnote_index = e.textContent
         //console.log(footnote_index);
 
-        let footnote_text_node = document.getElementById(`fn${footnote_index}`);
-        let footnote_text = '';
+        let footnote_text_node = document.getElementById(`fn${footnote_index}`)
+        let footnote_text = ''
 
-        if (!footnote_text_node){
-            footnote_text_node = document.getElementById(`fn:${footnote_index}`);
+        if (!footnote_text_node) {
+            footnote_text_node = document.getElementById(`fn:${footnote_index}`)
         }
 
-        if (!footnote_text_node){
-            console.log('can not found the target footnote content.');
+        if (!footnote_text_node) {
+            console.log('can not found the target footnote content.')
             return -1;
-        }else{
+        } else {
             footnote_text = footnote_text_node.textContent;
         }
 
-        $('sup',this).remove();
-        $(this).attr('data-bs-toggle','popover');
-        $(this).html(`<sup>注${footnote_index}</sup>`);
-        $(this).attr('data-bs-trigger','focus');
-        $(this).attr('data-bs-content',`${footnote_text}`);
-        $(this).attr('role','button');
-        $(this).attr('tabindex','0');
-        $(this).removeAttr('href');
-        $(this).attr('data-bs-placement',"bottom");
-    
+        e.innerHTML  = `<sup>注${footnote_index}</sup>`
+        e.setAttribute("data-bs-toggle", "popover")
+        e.setAttribute("data-bs-trigger", "focus")
+        e.setAttribute("data-bs-content", `${footnote_text}`)
+        e.setAttribute("role", "button")
+        e.setAttribute("tabindex", "0")
+        e.setAttribute("data-bs-placement", "bottom")
+        e.removeAttribute("href")
+    }
+
+    let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        let popover =  new bootstrap.Popover(popoverTriggerEl, {})
     })
-    
-    options = {};
-    $('.footnote-ref').popover(options);
+}
+
+function initArticle() {
+    add_table_class()
+    add_footnote_support()
+}
+
+if (document.readyState !== 'loading') {
+    initArticle()
+} else {
+    document.addEventListener('DOMContentLoaded', initArticle)
 }
 
 
-$(document).ready(function () {
-    adjust_markdown_css();
-    footnote_popover();
-});
 
 
 
